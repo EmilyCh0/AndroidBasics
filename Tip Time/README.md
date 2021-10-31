0b4a8d3880bd3796bf64a71ab86e251701c631e2
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-10-31 17:44:59 +0900

    pathway 2

--- Tip Time/README.md
+++ Tip Time/README.md
@@ -1,9 +1,9 @@
 ## [Unit2 pathway 1] 
 
-#### 👉 [구체적인 학습 내용 정리](https://velog.io/@emily2307/Unit-2-Layouts-1) 👈<br>
+#### 👉 [구체적인 학습 내용 정리 1](https://velog.io/@emily2307/Unit-2-Layouts-1) 👈<br>👉 [구체적인 학습 내용 정리 2](https://velog.io/@emily2307/Unit-2-Layouts-2) 👈<br>
 
-사용자가 입력한 금액과 선택한 서비스 평가에 따라 팁을 계산해 알려주는 어플<br>사용자가 사용하는 통화 단위에 따라 표기되며 아무것도 입력하지 않은 경우 null값을 처리한다.
+사용자가 입력한 금액과 선택한 서비스 평가에 따라 팁을 계산해 알려주는 어플
 
-<img src="images/tiptime.jpg" width="250"/>
+<img src="images/main.png" width="500"/>
 
 #### 
--- Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -14,32 +18,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateBtn.setOnClickListener{ calculateTip() }
+        binding.calculateBtn.setOnClickListener { calculateTip() }
+
+        // binding.costOfServiceEditText.setOnKeyListener(view, keyCode, _ -> handleKeyEvent(view, keyCode))
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         // 입력이 null인 경우 비정상 종료 방지
         val cost = stringInTextField.toDoubleOrNull()
-        if(cost==null){
+        if (cost == null) {
             binding.tipResult.text = ""
             return
         }
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId){
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_amazing -> 0.20
             R.id.option_good -> 0.18
             else -> 0.15
         }
 
-        var tip = tipPercentage*cost // 반올림 옵션이 있기 때문에 var
+        var tip = tipPercentage * cost // 반올림 옵션이 있기 때문에 var
 
-        if(binding.roundUpSwitch.isChecked){
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
 
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
 
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
