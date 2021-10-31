1c2a287c0887dbeed41bafb6969fd78bef57eb31
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-10-31 13:22:43 +0900

    tip time 개발 완료

--- Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateBtn.setOnClickListener{ calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        // 입력이 null인 경우 비정상 종료 방지
+        val cost = stringInTextField.toDoubleOrNull()
+        if(cost==null){
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId){
+            R.id.option_amazing -> 0.20
+            R.id.option_good -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage*cost // 반올림 옵션이 있기 때문에 var
+
+        if(binding.roundUpSwitch.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
